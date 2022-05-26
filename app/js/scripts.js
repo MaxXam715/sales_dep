7d1f5f2693207b427aff5717d4340c06204e6ffa
Max <gorbatenkomax.formula@gmail.com>
2022-05-27 00:19:52 +0300

    update

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -21,10 +21,7 @@ $(document).ready(function () {
         });
     });
 
-
-
-
-    $('.owl-carousel').owlCarousel({
+    $('.slider-projects').owlCarousel({
         loop:true,
         items: 1,
         margin: 10,
@@ -32,7 +29,39 @@ $(document).ready(function () {
         dots: true
     });
 
+    $('.partner-slider').owlCarousel({
+        loop:true,
+        margin: 20,
+        nav: false,
+        dots: true,
+        responsive : {
+            0 : {
+                items: 2,
+                margin: 0,
+            },
+            1100 : {
+                items: 4
+            }
+        }
+    });
 
+    $('.slider-reviews').owlCarousel({
+        loop:true,
+        margin: 20,
+        nav: false,
+        dots: true,
+        responsive : {
+            0 : {
+                items: 1,
+                margin: 0,
+            },
+            1100 : {
+                items: 3
+            }
+        }
+    });
+
+    // навигация для owl-slider
     $('.slider-control .btn').click(function () {
         var path = $(this).closest('section').find('.owl-carousel');
        if ($(this).hasClass('btn-prev')) {
@@ -40,17 +69,49 @@ $(document).ready(function () {
        } else if ($(this).hasClass('btn-next')) {
            path.find('.owl-next').click();
        }
-    })
+    });
+
+    // показать скрыть весь текст статьи
+    $('.toggle-text').click(function(){
+        var btn = $(this).closest('.item').find('.btn');
+        $(this).closest('.item').find('.btn').toggleClass('open');
+        $(this).closest('.item').find('.desc-all').slideToggle('fast');
+        if ($('.toggle-text').hasClass('open')) {
+            btn.html('Скрыть');
+        } else {
+            btn.html('Подробнее');
+        }
+        return false
+    });
+
+    // показать скрыть весь текст FAQ
+    $('.section-faq .item-faq').click(function(){
+        var $this = $(this)
+        $(this).toggleClass('open');
+        $(this).find('.answer').slideToggle('fast')
+        if ($('.section-faq .item-faq').hasClass('open')) {
+            $this.find('.icon').removeClass('plus').addClass('minus');
+        } else {
+            $this.find('.icon').removeClass('minus').addClass('plus');
+        }
+        return false
+    });
+
 
 });
 
+// открыть навигацию сайта
 function openMobileMenu() {
     setTimeout( function () {
         $('.navigation-mobile').addClass('open');
     }, 10);
 }
 
+// закрыть навигацию сайта
 function closeMobileMenu() {
     $('.navigation-mobile').removeClass('open');
     $('body').removeClass('overLock');
 }
+
+
+
